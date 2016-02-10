f748e9a2176c1a7e64debee5587c667cd259c383
kenlima <kenlima2k@gmail.com>
2016-02-10 18:40:36 +0900

    add to gladlib

---
+++ src/coursera/javastudy1/week2/GladLibTest.java
@@ -0,0 +1,18 @@
+package coursera.javastudy1.week2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jwlee on 2016-02-10.
+ */
+public class GladLibTest {
+
+    @Test
+    public void testMakeStory() throws Exception {
+        GladLib dl = new GladLib();
+        dl.makeStory();
+
+    }
+}
