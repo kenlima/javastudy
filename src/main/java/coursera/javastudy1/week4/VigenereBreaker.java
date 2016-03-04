5bad122b1022affaa3e5f8da0788dbdb0650f2ef
jwlee <kenlima@wemakeprice.com>
2016-03-04 09:44:14 +0900

    complete VigenereBreaker

--- src/main/java/coursera/javastudy1/week4/VigenereBreaker.java
+++ src/main/java/coursera/javastudy1/week4/VigenereBreaker.java
@@ -1,5 +1,8 @@
 package coursera.javastudy1.week4;
 
+import coursera.javastudy1.week1.CaesarBreaker;
+import edu.duke.FileResource;
+
 /**
  * Created by jwlee on 2016. 3. 1..
  */
@@ -11,5 +14,26 @@ public class VigenereBreaker {
             sb.append(c);
         }
         return sb.toString();
+    }
+    public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
+        int[] keys = new int[klength];
+        for(int i=0;i<klength;i++) {
+           String sliceStr = sliceString(encrypted, i, klength);
+            CaesarBreaker cb = new CaesarBreaker();
+            int key = cb.getKey(sliceStr);
+            keys[i] = key;
+        }
+        return keys;
+    }
+
+    public void breakVigenere() {
+        FileResource fr = new FileResource();
+        String str = fr.asString();
+
+        int[] keys = tryKeyLength(str, 4, 'e');
+        VigenereCipher vc = new VigenereCipher(keys);
+        String ret = vc.decrypt(str);
+
+        System.out.println(ret);
     }
 }
--- src/test/java/coursera/javastudy1/week4/VigenereBreakerTest.java
+++ src/test/java/coursera/javastudy1/week4/VigenereBreakerTest.java
@@ -1,5 +1,6 @@
 package coursera.javastudy1.week4;
 
+import edu.duke.FileResource;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -48,4 +49,25 @@ public class VigenereBreakerTest {
         ret = vb.sliceString("abcdefghijklm", 4, 5);
         System.out.println(ret);
     }
+
+    @Test
+    public void testTryKeyLength() throws Exception {
+        FileResource fr = new FileResource();
+        VigenereBreaker vb = new VigenereBreaker();
+        int[] rets = vb.tryKeyLength(fr.asString(), 4, 'e');
+
+        for (int ret : rets) {
+            System.out.println(ret);
+        }
+
+
+    }
+
+    @Test
+    public void testBreakVigenere() throws Exception {
+
+        VigenereBreaker vb = new VigenereBreaker();
+        vb.breakVigenere();
+    }
+
 }
